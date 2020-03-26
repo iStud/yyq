c5b7217aba54cbdb00f4283f89c9f68b4e50ce7e
xbk <xiongbk@edutech.com.cn>
2020-03-26 16:02:26 +0800

    项目初始化

---
+++ yyq/yyq/Main/MainController.swift
@@ -0,0 +1,30 @@
+//
+//  MainController.swift
+//  yyq
+//
+//  Created by bk.xiong on 2020/3/26.
+//  Copyright © 2020 xiongbk. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
